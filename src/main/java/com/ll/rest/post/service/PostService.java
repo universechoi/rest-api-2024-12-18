f11c0793cdbf4a652d63cc0868ca6683babd3be5
universechoi <skye.of.universe@gmail.com>
2024-12-18 11:41:45 +0900

    feat: add PostService

---
+++ src/main/java/com/ll/rest/post/service/PostService.java
@@ -0,0 +1,11 @@
+package com.ll.rest.post.service;
+
+import com.ll.rest.post.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+}
