00df4149595c82ab8054baa312de32ff26354261
universechoi <skye.of.universe@gmail.com>
2024-12-22 16:14:59 +0900

    refactor: change variable names

--- src/main/java/com/ll/rest/post/PostDto.java
+++ src/main/java/com/ll/rest/post/PostDto.java
@@ -7,15 +7,15 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
-    private LocalDateTime creatDate;
-    private LocalDateTime modifyDate;
+    private LocalDateTime createdDateTime;
+    private LocalDateTime modifiedDateTime;
     private String title;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.creatDate = post.getCreatedAt();
-        this.modifyDate = post.getModifiedAt();
+        this.createdDateTime = post.getCreatedAt();
+        this.modifiedDateTime = post.getModifiedAt();
         this.title = post.getTitle();
         this.content = post.getContent();
     }
